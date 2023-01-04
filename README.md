a03fa3c8a0903b15466d5cfdb1624f51ab1c9260
IsaacBreen <mail@isaacbreen.com>
2023-01-04 14:22:11 +0800

    add md2dir and expand readme

--- README.md
+++ README.md
@@ -1,24 +1,50 @@
-# dir2md
+# dir2md 
 
-`dir2md` is a command line utility for creating a markdown file that includes code blocks for all specified files.
+`dir2md` is a command-line tool that converts directories of files into Markdown code blocks. It also provides a reverse function, md2dir, which converts Markdown code blocks back into their original files.
 
-## Installation
+## Installation 
 
-Install dir2md using pip:
+Install `dir2md` using `pip`: 
 
-```bash
-pip install dir2md
+```bash 
+pip install dir2md 
 ```
 
 ## Usage
 
-To use `dir2md`, pass a list of file paths as arguments:
+`dir2md` can be used as a command-line tool or imported as a module.
+
+### Command-Line Tool
+
+To convert a directory of files to Markdown code blocks, run:
 
 ```bash
-dir2md file1.py file2.py
+dir2md [files...]
 ```
 
-This will output a markdown file with code blocks for `file1.py` and `file2.py`.
+This will print the resulting Markdown to the console.
+
+To convert Markdown code blocks back into their original files, run:
+
+```bash
+md2dir [options] <input_file>
+```
+
+This will create the files in the current working directory.
+
+For more options and usage details, use the `--help` flag.
+
+### Module
+
+```bash
+import dir2md
+
+# Convert a directory of files to Markdown code blocks
+markdown = dir2md.dir2md("file1.py", "file2.py")
+
+# Convert Markdown code blocks back into their original files
+dir2md.md2dir_save(markdown, output_dir="output/")
+```
 
 ### Wildcard support
 
@@ -38,11 +64,3 @@ dir2md **/*.py
 
 Note that the wildcard statement only works if it is expanded by the shell before the command is run. This means that you must use it in the command line or in a shell script, and it will not work if you pass it as a string to a function that runs the command.
 
-
-## Options
-
-Use the `--help` flag to view the available options:
-
-```bash
-dir2md --help
-```
--- dir2md.py
+++ dir2md.py
@@ -1,4 +1,6 @@
-import os
+import os, re, pathlib
+from typing import NamedTuple, Generator
+import fire
 
 
 def dir2md(*files: str) -> str:
@@ -15,11 +17,71 @@ def dir2md(*files: str) -> str:
             yield "```"
 
 
-def main():
-    import fire
+class TextFile(NamedTuple):
+    text: str
+    path: str
 
+
+def md2dir(text: str) -> Generator[TextFile, None, None]:
+    # Split the text into lines
+    lines = text.splitlines()
+    # Iterate over the lines
+    iter_lines = iter(lines)
+    for line in iter_lines:
+        # If the line is a comment
+        if line.startswith("<!-- ") and line.endswith(" -->"):
+            # Extract the path from the comment
+            path = line[5:-4]
+            # Get the number of ticks in the code fence opening
+            ticks = re.match("`*", next(iter_lines)).group()
+            # Get lines up until the closing code fence
+            code = []
+            for line in iter_lines:
+                if line.startswith(ticks):
+                    break
+                code.append(line)
+            # Yield the file
+            yield TextFile("\n".join(code), path)
+
+
+def md2dir_save(text: str, output_dir: str, yes: bool = False) -> None:
+    files = list(md2dir(text))
+    if not yes:
+        filenames = [file.path for file in files]
+        new_directories = []
+        for filename in filenames:
+            directory = os.path.dirname(filename)
+            if directory and not os.path.exists(directory) and directory not in new_directories:
+                new_directories.append(directory)
+        new_files = [file for file in filenames if not pathlib.Path(output_dir, file).exists()]
+        existing_files = [file for file in filenames if pathlib.Path(output_dir, file).exists()]
+        if new_directories:
+            print("The following directories will be created:")
+            for directory in new_directories:
+                print(f"  {directory!r}")
+        if new_files:
+            print("The following files will be created:")
+            for file in new_files:
+                print(f"  {pathlib.Path(output_dir, file)}")
+        if existing_files:
+            print("The following files will be overwritten:")
+            for file in existing_files:
+                print(f"  {pathlib.Path(output_dir, file)}")
+        print("Continue? (y/n)")
+        if input() != "y":
+            print("Aborted.")
+            return
+    for file in files:
+        print(f"Writing {pathlib.Path(output_dir, file.path)}")
+        path = pathlib.Path(output_dir, file.path)
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with open(path, "w") as f:
+            f.write(file.text)
+
+
+def dir2md_cli() -> None:
     fire.Fire(dir2md)
 
 
-if __name__ == "__main__":
-    main()
+def md2dir_cli() -> None:
+    fire.Fire(md2dir_save)

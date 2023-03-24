cb581cc0cf3e7a225002342132348ea6e3efca20
Isaac Breen <isaac.breen@westpac.com.au>
2023-03-24 16:39:56 +0800

    Trying to make a better parser.

--- dir2md.py
+++ dir2md.py
@@ -3,10 +3,14 @@ from __future__ import annotations
 import os
 import pathlib
 import re
-from typing import Generator, Callable
+from typing import Callable
+from typing import Generator, List
 from typing import NamedTuple
 
 import fire
+from funcparserlib.lexer import make_tokenizer, TokenSpec, Token
+from funcparserlib.parser import many
+from funcparserlib.parser import some, skip, finished
 
 
 class TextFile(NamedTuple):
@@ -29,34 +33,74 @@ def default_formatter(text_file: TextFile) -> str:
     r += ticks
     return r
 
+import re
+from typing import List
+from dataclasses import dataclass
 
-def default_parser(text: str) -> Generator[TextFile, None, None]:
-    # Split the text into lines
-    lines = text.splitlines()
-    # Iterate over the lines
-    iter_lines = iter(lines)
-    for line in iter_lines:
-        # If the line is a comment
-        if line.startswith("<!-- ") and line.endswith(" -->"):
-            # Extract the path from the comment
-            path = line[5:-4]
-            # Get the number of ticks in the code fence opening
-            ticks_match = re.match(r"(`+)", next(iter_lines, ""))
-            if ticks_match is None:
-                raise ValueError(f"Expected code fence opening after {path!r}.\ntext=\"\"\"\n{text}\n\"\"\"")
-            ticks = ticks_match.group(1)
-            # Get lines up to the closing code fence
-            code = []
-            for line in iter_lines:
-                if line.startswith(ticks):
-                    break
-                code.append(line)
+from funcparserlib.parser import some, Parser, finished, maybe
+
+@dataclass
+class TextFile:
+    path: str
+    text: str
+    
+def custom_tokenize(s: str) -> List[Token]:
+    tokens = []
+    in_code_block = False
+    open_ticks = None
+    while s:
+        if not in_code_block:
+            open_code_match = re.search(r"(\n^\s*(.+)\s*\n\s*(```+)\s*(\w*)\s*(\n|$)", s, flags=re.MULTILINE)
+            if open_code_match:
+                text = s[:open_code_match.start()]
+                path = open_code_match.group(1).strip()
+                open_ticks = open_code_match.group(2).strip()
+                print(text)
+                format_specifier = open_code_match.group(3).strip()
+                tokens.append(Token("text", text))
+                tokens.append(Token("path", path))
+                tokens.append(Token("OPEN_CODE_BLOCK", open_ticks))
+                s = s[open_code_match.end():]
+                in_code_block = True
             else:
-                # If the code fence was never closed, yield the partial file
-                yield TextFile("\n".join(code), path, partial=True)
-                return
-            # Yield the file
-            yield TextFile("\n".join(code), path, partial=False)
+                tokens.append(Token("text", s))
+                s = ""
+        else:
+            close_code_match = re.search(rf"\n\s*({open_ticks})\s*\n", s, flags=re.MULTILINE)
+            if close_code_match and close_code_match.group(1) == open_ticks:
+                text = s[:close_code_match.start()]
+                close_ticks = close_code_match.group(1).strip()
+                tokens.append(Token("text", text))
+                tokens.append(Token("CLOSE_CODE_BLOCK", close_ticks))
+                s = s[close_code_match.end():]
+                in_code_block = False
+            else:
+                tokens.append(Token("text", s))
+                s = ""
+
+    return tokens
+
+path_token = some(lambda t: t.type == "path").named("path")
+open_code_token = some(lambda t: t.type == "OPEN_CODE_BLOCK").named("OPEN_CODE_BLOCK")
+text_token = some(lambda t: t.type == "text").named("text")
+close_code_token = some(lambda t: t.type == "CLOSE_CODE_BLOCK").named("CLOSE_CODE_BLOCK")
+
+def to_text_file(path: str, open_code: str, text: List[str], close_code: str) -> TextFile:
+    # Combine text into a single string and remove code fence ticks
+    text = ''.join(text)[len(open_code):-len(close_code)]
+    return TextFile(path, text)
+
+default_parser = (
+    path_token
+    + open_code_token
+    + many(text_token | close_code_token)
+    + close_code_token
+    >> to_text_file
+) + -finished
+
+def parse(s: str) -> TextFile:
+    tokens = custom_tokenize(s)
+    return default_parser.parse(tokens)
 
 
 def dir2md(*files: str, formatter: str | Callable[[TextFile], str] = default_formatter) -> Generator[str, None, None]:
@@ -69,8 +113,10 @@ def dir2md(*files: str, formatter: str | Callable[[TextFile], str] = default_for
         yield from formatter(TextFile(text=code, path=file, partial=False)).splitlines()
 
 
-def md2dir(text: str, *, parser: Callable[[str], Generator[TextFile, None, None]] = default_parser()) -> Generator[TextFile, None, None]:
+def md2dir(text: str, *, parser: Callable[[str], Generator[TextFile, None, None]] = default_parser) -> Generator[
+    TextFile, None, None]:
     yield from parser(text)
+
 
 def save_dir(files: list[TextFile], output_dir: str, yes: bool = False) -> None:
     if files and not yes:
